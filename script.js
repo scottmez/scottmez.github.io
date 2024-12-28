e32006ee7e2e5a738ff5aca8ce9717debb86b805
scottmez <scottmesdjian10@gmail.com>
2024-12-27 16:15:31 -0800

    site rework pt.1

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-/*const root = document.documentElement;
 
-function toggleTheme() {
-  const isDarkMode = root.classList.toggle('dark-mode');
 
-  // Check for browser support of prefers-color-scheme
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches === isDarkMode) {
-    // Respect user preference
-    return;
-  }
-
-  // Set theme based on time of day
-  const now = new Date().getHours();
-  const isNight = now >= 18 || now <= 6; // Adjustable time range for dark mode
-
-  if (isNight != isDarkMode){
-    root.classList.toggle('dark-mode');
-  }
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
-// Initial theme setup
-toggleTheme();
-
-// Respect user preference if available
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleTheme);*/
